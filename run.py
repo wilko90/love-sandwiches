16d400ce23c00f7e21c5794fdc432b426c1d1914
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-06-01 13:07:01 +0000

    add user request loop

--- run.py
+++ run.py
@@ -16,14 +16,20 @@ def get_sales_data():
     ''''
     get sales input from user
     '''
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print('data is valid')
+            break
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -32,14 +38,15 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
 
-    
-    print(values)
 
-get_sales_data()
+data = get_sales_data()
